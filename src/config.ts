58a93595340c2bd0d88c66ef6dec353279a45047
李兴鹏 <asd3245908066@outlook.com>
2026-02-02 14:27:22 +0800

    fix

--- src/config.ts
+++ src/config.ts
@@ -9,8 +9,8 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "Arthur的个人小站",
-	subtitle: "欢迎！！！",
-	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	subtitle: "欢迎",
+	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
@@ -44,11 +44,6 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Home,
 		LinkPreset.Archive,
 		LinkPreset.About,
-		{
-			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
-			external: true, // Show an external link icon and will open in a new tab
-		},
 	],
 };
 
